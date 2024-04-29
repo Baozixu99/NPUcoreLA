561c6443a300c433091e0cd44d8d66f583b2a504
BZX <770090546@qq.com>
2024-04-29 19:21:51 +0800

    delete

--- util/os/src/arch/la64/register/timer/mod.rs
+++
@@ -1,6 +0,0 @@
-#![allow(unused)]
-pub mod cntc;
-pub mod tcfg;
-pub mod ticlr;
-pub mod tid;
-pub mod tval;
--- util/os/src/arch/la64/switch.rs
+++
@@ -1,8 +0,0 @@
-use crate::task::TaskContext;
-use core::arch::global_asm;
-
-global_asm!(include_str!("switch.S"));
-
-extern "C" {
-    pub fn __switch(current_task_cx_ptr: *mut TaskContext, next_task_cx_ptr: *const TaskContext);
-}
