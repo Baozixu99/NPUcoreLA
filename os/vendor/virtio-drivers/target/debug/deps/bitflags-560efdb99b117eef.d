f1e55f0a253980c3e9c9fbd17bdc426fd11b4d3c
BZX <770090546@qq.com>
2024-08-14 00:25:55 +0800

    测试平台提交，依赖.cargo文件

---
+++ os/vendor/virtio-drivers/target/debug/deps/bitflags-560efdb99b117eef.d
@@ -0,0 +1,7 @@
+/home/b/test/virtio-drivers/target/debug/deps/libbitflags-560efdb99b117eef.rmeta: /home/b/.cargo/registry/src/index.crates.io-6f17d22bba15001f/bitflags-1.3.2/src/lib.rs
+
+/home/b/test/virtio-drivers/target/debug/deps/libbitflags-560efdb99b117eef.rlib: /home/b/.cargo/registry/src/index.crates.io-6f17d22bba15001f/bitflags-1.3.2/src/lib.rs
+
+/home/b/test/virtio-drivers/target/debug/deps/bitflags-560efdb99b117eef.d: /home/b/.cargo/registry/src/index.crates.io-6f17d22bba15001f/bitflags-1.3.2/src/lib.rs
+
+/home/b/.cargo/registry/src/index.crates.io-6f17d22bba15001f/bitflags-1.3.2/src/lib.rs:
---
+++ os/vendor/virtio-drivers/target/debug/deps/log-b6b1a89bf87ced53.d
@@ -0,0 +1,10 @@
+/home/b/test/virtio-drivers/target/debug/deps/liblog-b6b1a89bf87ced53.rmeta: /home/b/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.22/src/lib.rs /home/b/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.22/src/macros.rs /home/b/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.22/src/serde.rs /home/b/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.22/src/__private_api.rs
+
+/home/b/test/virtio-drivers/target/debug/deps/liblog-b6b1a89bf87ced53.rlib: /home/b/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.22/src/lib.rs /home/b/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.22/src/macros.rs /home/b/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.22/src/serde.rs /home/b/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.22/src/__private_api.rs
+
+/home/b/test/virtio-drivers/target/debug/deps/log-b6b1a89bf87ced53.d: /home/b/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.22/src/lib.rs /home/b/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.22/src/macros.rs /home/b/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.22/src/serde.rs /home/b/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.22/src/__private_api.rs
+
+/home/b/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.22/src/lib.rs:
+/home/b/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.22/src/macros.rs:
+/home/b/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.22/src/serde.rs:
+/home/b/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.22/src/__private_api.rs:
