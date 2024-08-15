1708c64f5c3102d64d215d2ef09e4cb1df19b3a7
BZX <770090546@qq.com>
2024-08-15 19:47:06 +0800

    最终测试平台提交，依赖.cargo文件

---
+++ os/vendor/virtio-drivers/target/debug/deps/volatile-549a73781b0b679f.d
@@ -0,0 +1,7 @@
+/home/b/test/virtio-drivers/target/debug/deps/libvolatile-549a73781b0b679f.rmeta: /home/b/.cargo/registry/src/index.crates.io-6f17d22bba15001f/volatile-0.3.0/src/lib.rs
+
+/home/b/test/virtio-drivers/target/debug/deps/libvolatile-549a73781b0b679f.rlib: /home/b/.cargo/registry/src/index.crates.io-6f17d22bba15001f/volatile-0.3.0/src/lib.rs
+
+/home/b/test/virtio-drivers/target/debug/deps/volatile-549a73781b0b679f.d: /home/b/.cargo/registry/src/index.crates.io-6f17d22bba15001f/volatile-0.3.0/src/lib.rs
+
+/home/b/.cargo/registry/src/index.crates.io-6f17d22bba15001f/volatile-0.3.0/src/lib.rs:
