c912c192a865ccabbac631e4dac23c3e180667d6
BZX <770090546@qq.com>
2024-08-19 16:34:34 +0800

    add user/splice-test

--- user/src/bin/initproc.rs
+++ user/src/bin/initproc.rs
@@ -30,12 +30,12 @@ fn main() -> i32 {
     ];
     sleep(9000) ;
     let schedule_text: &str= "
-    echo bbb > lat_sig\0
     ./time-test\0
     ./busybox_testcode.sh\0
     ./lua_testcode.sh\0
+    ./splice-test-5\0
     ./lmbench_testcode.sh\0
-    ./libc-bench\0
+     
     ";
     let mut exit_code: i32 = 0;
     for line in schedule_text.lines(){
@@ -57,8 +57,8 @@ fn main() -> i32 {
     // let mut exit_code: i32 = 0;
     // let pid = fork();
     // if pid == 0 {
-    //     exec(path, &[path.as_ptr() as *const u8, "-c\0".as_ptr(), "./run-all.sh\0".as_ptr(), core::ptr::null()], &environ);
-    //     //exec(path, &[path.as_ptr() as *const u8, core::ptr::null()], &environ);
+    //     //exec(path, &[path.as_ptr() as *const u8, "-c\0".as_ptr(), "./run-all.sh\0".as_ptr(), core::ptr::null()], &environ);
+    //     exec(path, &[path.as_ptr() as *const u8, core::ptr::null()], &environ);
     // } else {
     //     waitpid(pid as usize, &mut exit_code);
     // }
